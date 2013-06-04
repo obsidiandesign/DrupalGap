58728636942ec4e68b490b8d85ce8e4c01197fe3
Tyler Frankenstein <tyler.frankenstein@gmail.com>
2013-06-04 18:53:21 -0400

    Changed drupalgap_mvc_init() to attach an id, module, and type variables to the model JSON object, and create an empty collection for that models items. Changed drupalgap_mvc_model_load() to set its form elements to model.fields. Added basics to item_save() and changed drupalgap_mvc_model_create_form_submit() to send along the form state values to item_save().

--- includes/mvc.inc.js
+++ includes/mvc.inc.js
@@ -9,7 +9,7 @@ function drupalgap_mvc_init() {
     // Load models...
     // For each module that implements hook_mvc_model(), iterate over each model
     // placing it into drupalgap.mvc.models, each model will be placed into
-    // a namespace according to its implementing module to avoid namespace
+    // a namespace according to the module that implements it, to avoid namespace
     // collisions.
     var modules = module_implements('mvc_model');
     for (var i = 0; i < modules.length; i++) {
@@ -20,12 +20,36 @@ function drupalgap_mvc_init() {
         if (!eval('drupalgap.mvc.models.' + module)) {
           eval('drupalgap.mvc.models.' + module + ' = {};');
         }
-        // Add each model to its namespace within drupalgap.mvc.models
-        $.each(models, function(model_name, model){
-            eval('drupalgap.mvc.models.' + module + '.' + model_name + ' = model;');
+        // For each model type...
+        $.each(models, function(model_type, model){
+            // Set the primary key 'id', the module name, and model type
+            // on the model fields.
+            model.fields.id = {
+              "type":"hidden",
+              "title":"ID",
+              "required":false
+            };
+            model.fields.module = {
+              "type":"hidden",
+              "title":"Module",
+              "required":true,
+              "default_value":module,
+            };
+            model.fields.type = {
+              "type":"hidden",
+              "title":"Model Type",
+              "required":true,
+              "default_value":model_type,
+            };
+            // Add each model type to its namespace within drupalgap.mvc.models
+            eval('drupalgap.mvc.models.' + module + '.' + model_type + ' = model;');
+            // Save an empty item collection to local storage.
+            window.localStorage.setItem('items_' + module + '_' + model_type, '[]');
         });
       }
     }
+    //console.log(JSON.stringify(drupalgap.mvc.models));
+    //alert('drupalgap_mvc_init');
     // These may not be needed. Perhaps we should just call assumed hooks that
     // should be implemented for any custom views and controllers at the time
     // they are needed, probably no need to bundle them inside drupalgap.mvc.
@@ -41,13 +65,14 @@ function drupalgap_mvc_init() {
 // model_load();
 // model_save();
 // model_delete();
-// model_item_load();
-// model_item_save();
-// model_item_delete();
+// item_load();
+// item_save();
+// item_delete();
 // etc...
 
 /**
- *
+ * Given a module name and a corresponding model name, this will load the model
+ * from drupalgap.mvc.models.
  */
 function drupalgap_mvc_model_load(module, name) {
   try {
@@ -78,7 +103,7 @@ function drupalgap_mvc_model_create_form(module, name) {
     if (model) {
       form = {
         id:"drupalgap_mvc_model_create_form",
-        elements:model
+        elements:model.fields
       };
       form.buttons = {
         cancel:{"title":"Cancel"}
@@ -95,7 +120,47 @@ function drupalgap_mvc_model_create_form(module, name) {
   }
 }
 
+/**
+ *
+ */
+/*function drupalgap_mvc_model_create_form_validate(form, form_state) {
+  
+}*/
+
+/**
+ *
+ */
 function drupalgap_mvc_model_create_form_submit(form, form_state) {
-  alert('shweeeet');
+  try {
+    console.log(JSON.stringify(form));
+    console.log(JSON.stringify(form_state));
+    item_save(form_state.values);
+    alert('submitting...');
+  }
+  catch (error) {
+    alert('drupalgap_mvc_model_create_form_submit - ' + error);
+  }
+}
+
+/**
+ *
+ */
+function item_save(item) {
+  try {
+    if (typeof item === 'undefined') { return false; }
+    // If there is no id, then this is a new item, generate an item id.
+    if (!item.id) {
+      // We'll set the new item id to the UTC in seconds.
+      var d = new Date();
+      item.id = d.getTime()/1000;
+    }
+    // Save the item to its local storage collection.
+    //window.localStorage.setItem('items_' + item.module + '_' + item.type item.id, JSON.stringify(item));
+    // Save the item id in the model's item collection.
+    return true;  
+  }
+  catch (error) {
+    alert('item_save - ' + error);
+  }
 }
 
